89d88133a46e8f253522ab288e981c832d25679a
SELLER Peter Andras <spa@palette-software.com>
2016-12-01 17:59:16 +0100

    Fix comment indent

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-  # Upload the RPM to the RPM repository
-  # by exportin it to SSHPASS, sshpass wont log the command line and the password
+# Upload the RPM to the RPM repository
+# by exportin it to SSHPASS, sshpass wont log the command line and the password
 export SSHPASS=$DEPLOY_PASS
 sshpass -e scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r rpm-build/_build/* $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 
